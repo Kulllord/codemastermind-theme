ee6052570a2dae9a6f61f1ac2afc289bd9b3a6e0
Kulllord <kulllord@gmail.com>
2016-03-17 18:18:54 -0500

    changing site title

--- header.php
+++ header.php
@@ -30,19 +30,13 @@ $postID = $post->ID;
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container">
 			<div class="site-branding">
-				<?php
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif;
-
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' );
+				?></a><?php
 				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-				endif; ?>
+				if ( $description || is_customize_preview() ) :
+					?><br><span class="site-description small"><?php echo $description; /* WPCS: xss ok. */
+				?></span><?php
+				endif; ?></h1>
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
